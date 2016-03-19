cb2715fa01280a3860b7eedc5a1894b433de24ff
Christopher Bishop <domingobishop@gmail.com>
2016-03-19 11:45:59 +0000

    removed fluid container

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <body <?php body_class(); ?>>
     <header id="head" class="bc-head">
         <nav class="navbar navbar-default">
-            <div class="container-fluid">
+            <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
